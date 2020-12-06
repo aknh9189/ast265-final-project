f74c2969a24eacf7fa8a2d0e4f19fd5c219dc3c3
Ethan Fahnestock <efahnest@u.rochester.edu>
2020-12-05 21:54:29 -0500

    Adding initial files

---
+++ monitor_runs.py
@@ -0,0 +1,36 @@
+from glob import glob
+import os 
+import time
+import subprocess
+import sys
+last_tt = time.time()
+last_percent = {}
+while True:
+    directories = glob("*/")
+    output = ""
+    for fi in directories:
+        if os.path.exists(fi  +'Ice/output.txt0'):
+            last_time = os.stat(fi+'Ice/output.txt0').st_mtime
+            t = time.time()
+            dt = t-last_tt
+            last_tt = t
+            if t - last_time > 30:
+                continue
+            with open(fi + "Ice/output.txt0", 'r') as f:
+                data = f.read()
+            last_idx = data.rfind('PERCENT')
+            if last_idx != -1:
+                trimmed = data[last_idx:]
+                trimmed = trimmed.split("\n")[0]
+                trimmed = trimmed.split(" ")[-1]
+                perc = float(trimmed[:-1])
+                eta = 0.0
+                if fi in last_percent and perc - last_percent[fi] > 0:
+                    eta = (100 - perc) / ((perc - last_percent[fi]) / dt) / 60
+                last_percent[fi] = perc
+                output += fi + " " + trimmed +  "\t"
+    sys.stdout.write("\r" + output)
+    sys.stdout.flush()
+    time.sleep(1)
+
+
---
+++ runJob.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+
+import argparse, os, shutil, sys, yaml
+
+data_dir = './data'
+
+parser = argparse.ArgumentParser(description="tmp")
+parser.add_argument('-y', '--yamlfile', type=str, required=True)
+
+args = parser.parse_args()
+
+with open(args.yamlfile, 'r') as f:
+    data = yaml.load(f)
+print(data)
+
+template_dir = data['system']['template_dir']
+
+def create_job(job):
+    job_id = job['id']
+    variables = job['variables']
+    shutil.copytree(template_dir, job_id)
+    curdur = os.getcwd()
+    os.chdir(job_id)
+    f = open("asteroid.inp", 'r+')
+    text = f.read()
+    for v in variables:
+        linestart = text.find(v)
+        lineend = text.find("\n", linestart)
+        line = text[linestart:lineend]
+        parts = line.split(":")
+        for i in range(len(variables[v])): # for each value to set 
+            part = parts[i+1]
+            spaces = part.split(" ")
+            max_i = spaces.index(max(spaces, key=len))
+            spaces[max_i] = variables[v][i]
+            parts[i+1] = " ".join(spaces)
+            
+        newline = ":".join(parts)
+        text = text.replace(line, newline)
+    f.seek(0)
+    f.write(text)
+    f.close()
+    os.chdir(curdur)
+
+def run_job(job)
+    curdur = os.getcwd()
+    os.chdir(job['id'])
+    os.system('./iSALE2D')
+    os.chdir(curdur)
+    os.chdir("ast265-final-data")
+    os.system("./newresult -p ../{} -n {}".format(job['id'], job['id']))
+    os.chdir(curdur)
+
+def cleanup_job(job):
+    shutil.rmtree(job['id'])
+
+def complete_job(job):
+    create_job(job)
+    run_job(job)
+    cleanup_job(job)
+
+
+
+
